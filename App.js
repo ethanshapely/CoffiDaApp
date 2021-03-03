8c09dba2b820bf0fcd119db9e0f760ba8f6ebf49
ethanshapely <ethan.shapely@gmail.com>
2021-03-03 17:16:45 +0000

    Removed some comments from the App.js file and am currently trying to push forward older requests that were unknowingly not recieved by the github repository

--- App.js
+++ App.js
@@ -1,14 +1,5 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
-
 import React, {Component} from 'react';
 import { SafeAreaView, StyleSheet, ScrollView, View, Text, StatusBar} from 'react-native';
-//import { Header, LearnMoreLinks, Colors, DebugInstructions, ReloadInstructions} from 'react-native/Libraries/NewAppScreen';
 
 import Home from './components/home';
 import Login from './components/login';
