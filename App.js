c016ce48967ac23c5a89f697466eba7de4cc5623
ethanshapely <ethan.shapely@gmail.com>
2021-03-05 16:08:25 +0000

    One last missing import from App.js

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import 'react-native-gesture-handler';
 import React, { Component } from 'react';
-import { SafeAreaView, StyleSheet, ScrollView, View, Text, StatusBar } from 'react-native';
+import { SafeAreaView, StyleSheet, ScrollView, View, Button, Text, StatusBar } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 
